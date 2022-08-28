c339671cd17c0dda3a999a540374938eb9a8afa7
AaronHuang <398838870@qq.com>
2022-08-28 16:38:02 +0800

    Alexnet

---
+++ AlexNet.py
@@ -0,0 +1,51 @@
+# 代码创作者： Aaron（黄炜）
+# 联系方式：aaronwei@buaa.edu.cn
+# 开发时间： 2022/8/28 16:32
+import torch
+import torch.nn as nn
+from torchinfo import summary
+
+
+class AlexNet(nn.Module):
+    def __init__(self, num_classes=1000):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 48, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(48, 128, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(128, 192, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(192, 192, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(192, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(128 * 6 * 6, 2048),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.2),
+            nn.Linear(2048, 2048),
+            nn.ReLU(inplace=True),
+            nn.Linear(2048, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        out = self.classifier(x)
+        return out
+
+
+def test():
+    net = AlexNet()
+    y = net(torch.randn(1, 3, 224, 224))
+    print(y.size())
+    summary(net, (1, 3, 224, 224))
+
+
+if __name__ == '__main__':
+    test()
